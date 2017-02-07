da47ad6a7eb10a2159a61059267245eac4b23b78
EDawg878 <EDawg878@gmail.com>
2017-02-06 23:54:59 -0500

    Remove gson from DownloadJSON

--- src/main/java/boilerhungry/backend/demo/DownloadJSON.java
+++ src/main/java/boilerhungry/backend/demo/DownloadJSON.java
@@ -1,10 +1,6 @@
 package boilerhungry.backend.demo;
 
 import boilerhungry.backend.DiningCourtAPI;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -23,10 +19,7 @@ public class DownloadJSON extends DiningCourtAPI {
         String date = "02-02-2017";
         String api = "https://api.hfs.purdue.edu/menus/v2/locations/" + diningCourt + "/" + date;
         JSONObject jsonObject = getJSON(new URL(api));
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        JsonParser jp = new JsonParser();
-        JsonElement je = jp.parse(jsonObject.toString());
-        String prettyJsonString = gson.toJson(je);
+        String prettyJsonString = jsonObject.toString(4);
         String file = dest + diningCourt + "-" + date + ".json";
         Files.write(Paths.get(file), prettyJsonString.getBytes());
         System.out.println("Saved " + file);
