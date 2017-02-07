f6b3a484a7fea362ec9b3dae775f00721d4210a4
EDawg878 <EDawg878@gmail.com>
2017-02-07 00:39:51 -0500

    Remove file dependency from MockDiningCourtAPI

--- src/main/java/boilerhungry/backend/mock/MockDiningCourtAPI.java
+++ src/main/java/boilerhungry/backend/mock/MockDiningCourtAPI.java
@@ -3,7 +3,6 @@ package boilerhungry.backend.mock;
 import boilerhungry.backend.DiningCourtAPI;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -11,12 +10,6 @@ import java.net.URL;
  * Created by eric on 2/7/17.
  */
 public class MockDiningCourtAPI implements DiningCourtAPI {
-
-    private File file;
-
-    public MockDiningCourtAPI(File file) {
-        this.file = file;
-    }
 
     @Override
     public JSONObject getJSON(URL url) throws IOException {
