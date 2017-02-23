1f84688e965398e7a6f6accaea59aa59e3d102cf
Matthew <mashbeck@purdue.edu>
2017-02-22 22:18:25 -0500

    Added init to HomeServlet, Added DietaryPreferencesServlet and DietaryPreferences.jsp

--- src/main/java/boilerhungry/backend/servlets/HomeServlet.java
+++ src/main/java/boilerhungry/backend/servlets/HomeServlet.java
@@ -2,18 +2,32 @@ package boilerhungry.backend.servlets;
 
 import boilerhungry.backend.DiningCourt;
 import boilerhungry.backend.DiningCourtAPI;
+import boilerhungry.backend.Settings;
 import boilerhungry.backend.purdue.PurdueDiningCourtAPI;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 
 public class HomeServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        try {
+            config.getServletContext().setAttribute("Settings", Settings.load(new File("settings.json")));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
