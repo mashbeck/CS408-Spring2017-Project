331141015a091b570f25ca303bfb698c6eb47a3c
EDawg878 <EDawg878@gmail.com>
2017-03-03 19:59:58 -0500

    Simplify NotificationsExample

--- src/main/java/boilerhungry/backend/ItemAppearance.java
+++ src/main/java/boilerhungry/backend/ItemAppearance.java
@@ -1,22 +1,27 @@
 package boilerhungry.backend;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 public class ItemAppearance {
 
-    private final String searchFood;
+    private final String foodName;
     private Optional<String> diningCourt;
     private Optional<String> station;
     private Optional<String> meal;
     private Optional<LocalDateTime> dateTime;
 
-    public ItemAppearance(String searchFood) {
-        this.searchFood = searchFood;
+    public ItemAppearance(String foodName) {
+        this.foodName = foodName;
         this.diningCourt = Optional.empty();
         this.station = Optional.empty();
         this.meal = Optional.empty();
         this.dateTime = Optional.empty();
+    }
+
+    public String getFoodName() {
+        return foodName;
     }
 
     public Optional<String> getStation() {
@@ -49,6 +54,16 @@ public class ItemAppearance {
 
     public void setDateTime(LocalDateTime date) {
         this.dateTime = Optional.ofNullable(date);
+    }
+
+    @Override
+    public String toString() {
+        String meal = getMeal().orElse("(unknown)");
+        String date = getDateTime().map(dt -> dt.format(DateTimeFormatter.ISO_LOCAL_DATE)).orElse("(unknown)");
+        String time = getDateTime().map(dt -> dt.format(DateTimeFormatter.ofPattern("h:mm a"))).orElse("(unknown)");
+        String station = getStation().orElse("(unknown)");
+        String diningCourt = getDiningCourt().orElse("(unknown)");
+        return String.format("%s appears at %s on %s at %s at the station %s during %s", foodName, diningCourt, date, time, station, meal);
     }
 
 }
--- src/main/java/boilerhungry/backend/demo/NotificationsExample.java
+++ src/main/java/boilerhungry/backend/demo/NotificationsExample.java
@@ -5,7 +5,6 @@ import boilerhungry.backend.purdue.PurdueDiningCourtAPI;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -19,16 +18,10 @@ public class NotificationsExample {
         Notifications notifications = new Notifications(api, settings);
         Map<String, List<UpcomingFood>> myFoodNotifications = notifications.getMyFoodAppearances();
         for (Map.Entry<String, List<UpcomingFood>> entry : myFoodNotifications.entrySet()) {
-            String name = entry.getKey();
             List<UpcomingFood> upcomingFoods = entry.getValue();
             for (UpcomingFood food : upcomingFoods) {
                 for (ItemAppearance appearance : food.getAppearances()) {
-                    String meal = appearance.getMeal().orElse("(unknown)");
-                    String date = appearance.getDateTime().map(dt -> dt.format(DateTimeFormatter.ISO_LOCAL_DATE)).orElse("(unknown)");
-                    String time = appearance.getDateTime().map(dt -> dt.format(DateTimeFormatter.ofPattern("h:mm a"))).orElse("(unknown)");
-                    String station = appearance.getStation().orElse("(unknown)");
-                    String diningCourt = appearance.getDiningCourt().orElse("(unknown)");
-                    System.out.printf("%s is found in %s on %s at %s at the station %s during %s\n", name, diningCourt, date, time, station, meal);
+                    System.out.println(appearance);
                 }
             }
         }
